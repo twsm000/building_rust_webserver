58fa3d3206d12c7619a77f149a53de6da8d9de2d
Thomas W. S. Moura <twsm000@gmail.com>
2023-05-24 11:02:27 -0300

    Routing Incoming Requests

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,15 +1,25 @@
 use crate::{
-    http::{Request, Response, StatusCode},
+    http::{Method, Request, Response, StatusCode},
     server::Handler,
 };
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
-    fn handle_request(&mut self, _request: &Request) -> Response {
-        Response::new(
-            StatusCode::Ok,
-            Some("<h1>Data sent by WebsiteHandler</h1>".to_string()),
-        )
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(
+                    StatusCode::Ok,
+                    Some("<h1>Welcome to my first Rust Website</h1>".to_string()),
+                ),
+                "/hello" => Response::new(
+                    StatusCode::Ok,
+                    Some("<h1>Hello</h1>".to_string()),
+                ),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
