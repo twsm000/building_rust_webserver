32836ef5eae5b1d4193f01c475066180773b08cf
Thomas W. S. Moura <twsm000@gmail.com>
2023-05-24 11:25:00 -0300

    Serving HTML files

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -9,8 +9,16 @@ pub struct WebsiteHandler {
 
 impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
-        println!("New WebsiteHandler reading files from path: {}", &public_path);
+        println!(
+            "New WebsiteHandler reading files from path: {}",
+            &public_path
+        );
         Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        std::fs::read_to_string(path).ok()
     }
 }
 
@@ -18,11 +26,8 @@ impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(
-                    StatusCode::Ok,
-                    Some("<h1>Welcome to my first Rust Website</h1>".to_string()),
-                ),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             },
             _ => Response::new(StatusCode::NotFound, None),
